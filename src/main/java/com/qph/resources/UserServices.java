7d5e4b79cb8cd35962d43adc0ba6ace02dc8e24a
phquoc25 <phquoc25@gmail.com>
2015-02-14 14:50:16 +0700

    Integrate log4j.

--- src/main/java/com/qph/resources/UserServices.java
+++ src/main/java/com/qph/resources/UserServices.java
@@ -4,13 +4,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
+
+
 
 @Path("/user")
 public class UserServices {
 	
+	private Logger logger = Logger.getLogger(UserServices.class);
 	@GET
 	@Path("/detail")
 	public Response getUser() {
+		logger.info("enter Userservice.getUser method");
 		return Response
 				   .status(200)
 				   .entity("getUsers is called").build();
